b67ad6215ee28fb4f9dab9108621aaba471dfc87
amf-mf777 <amf.mf777@gmail.com>
2025-10-04 12:35:24 +0530

    [add]

---
+++ mobile_service_shop_pro.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Amaya Aravind EV (odoo@cybrosys.com)
+#
+#    This program is under the terms of the Odoo Proprietary License v1.0 (OPL-1)
+#    It is forbidden to publish, distribute, sublicense, or sell copies of the
+#    Software or modified copies of the Software.
+#
+#    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+#    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,ARISING
+#    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#    DEALINGS IN THE SOFTWARE.
+#
+###############################################################################
+import json
+from odoo import http
+from odoo.http import content_disposition, request
+from odoo.tools import html_escape
+
+
+class XLSXReportController(http.Controller):
+    """Class of the controller. Creating a XLSXReportController to create
+    XLSXReport."""
+
+    @http.route('/mobile_service_xlsx_reports', type='http', auth='user', methods=['POST'],
+                csrf=False)
+    def get_report_xlsx(self, model, options, output_format, report_name):
+        """Creates the xlsx report and return the xlsx report."""
+        uid = request.session.uid
+        report_obj = request.env[model].with_user(uid)
+        options = json.loads(options)
+        token = 'dummy-because-api-expects-one'
+        try:
+            if output_format == 'xlsx':
+                response = request.make_response(
+                    None,
+                    headers=[
+                        ('Content-Type', 'application/vnd.ms-excel'),
+                        ('Content-Disposition',
+                         content_disposition(report_name + '.xlsx'))
+                    ]
+                )
+                report_obj.get_xlsx_report(options, response)
+                response.set_cookie('fileToken', token)
+                return response
+        except Exception as e:
+            error = {
+                'code': 200,
+                'message': 'Odoo Server Error',
+                'data': http.serialize_exception(e)
+            }
+            return request.make_response(html_escape(json.dumps(error)))
